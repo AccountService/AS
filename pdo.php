6d6aadbf6675e9b0bb99be35e0b279274b037fea
ptroyanov <paul.troyanov@gmail.com>
2015-06-10 20:02:29 +0300

    working validation

--- pdo.php
+++ pdo.php
@@ -8,18 +8,53 @@
         $query = $db->prepare("INSERT INTO generated_keys (gen_key) VALUES (:newkey)");
         $query->bindParam(':newkey',$new_key, PDO::PARAM_STR);
         $query->execute();
-        echo 'asd';
     }
 
-    function validateKey($db, $key) {
-        $query = $db->prepare("SELECT gen_key FROM generated_keys WHERE gen_key=:key");
-        $query->bindParam(':key',$key, PDO::PARAM_STR);
+
+    // Function that return an array of keys with "IS_MARKED" = false;
+    function getKeys($db) {                         
+        $keys = array();
+        $query = $db->prepare("SELECT gen_key FROM generated_keys WHERE is_marked = 0");
         $query->execute();
-        $valid_key = false;
-        if(fetch(PDO::FETCH_ASSOC)!=false) {
-            return true;
-        } else return false;
+        
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $keys[] = $row['gen_key'];
+        }
+        
+        return $keys;
     }
 
 
-?>
+    // Function that update 'is_marked' to 'true'
+    // and create connection between user 
+    // and key.
+    function markKey($key, $user_id, $db) {                                             
+        $query = $db->prepare("UPDATE generated_keys SET is_marked = 1 WHERE gen_key = :key");
+        $query->bindParam(':key', $key, PDO::PARAM_STR);
+        
+        $query->execute();
+
+        $query = $db->prepare("UPDATE generated_keys SET user_ID = :user_id WHERE gen_key = :key");
+        $query->bindParam(':key', $key, PDO::PARAM_STR);
+        $query->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        
+        $query->execute();
+    }
+
+
+
+
+    // Function that compare key from html form with NySQL keys. $key --> string;
+    function keyValidation($key) {              
+        $allKeys = getKeys(get_db_connect());           // returns all keys. getConnect() func. that connect to MySQL database;
+
+        if(in_array($key, $allKeys)) {
+            markKey($key, 1, get_db_connect());
+            echo "u're cool";
+            return true;
+        }
+        echo 'fail';
+        return false;
+    }
+
+   
