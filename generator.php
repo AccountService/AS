a4410270d41f3bbcaf9140ea11c1fe175e3fdd0b
Matthew <fantom79022@gmail.com>
2015-06-10 17:50:53 +0300

    add config to our db

--- generator.php
+++ generator.php
@@ -1,27 +1,25 @@
 
 <?php
-
-function generate_key() { //генерируем ключ по заданному алгоритму (получаем абослютно точно уникальный ключ)
-
-    $randomv = rand().rand().rand();
-    $new_key = hash('sha256', $randomv);
-    $new_key .= microtime(true);
-    return $new_key;
+    include_once('pdo.php');
+    function generate_key() { //генерируем ключ по заданному алгоритму (получаем абослютно точно уникальный ключ)
+        $randomv = rand().rand().rand();
+        $new_key = hash('sha256', $randomv);
+        $new_key .= microtime(true);
+        return $new_key;
 }
 
-function addtodb ($key){
-//подключаемся к базе данных
-//и добавляем ключ
+    function addtodb ($key){
+
     return true;
 }
-function createkeys($number) {
-    $i = 0;
-    while ($i<$number) {
-        $key=generate_key();
-        addtodb($key);
-        echo "$key<br>";
+    function createkeys($number) {
+     $i = 0;
+     while ($i<$number) {
+         $key=generate_key();
+         addkey(get_db_connect(),$key);
+         echo "$key<br>";
         $i++;
-    }
+     }
 
 }
 
