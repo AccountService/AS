8c6dc6a147ac130a4885263fc8b200309fb28b78
Matthew <fantom79022@gmail.com>
2015-06-11 18:37:49 +0300

    Генератор изменился

--- generator.php
+++ generator.php
@@ -2,19 +2,20 @@
 <?php
     include_once('pdo.php');
 //генерируем ключ по заданному алгоритму (получаем абослютно точно уникальный ключ)
-    function generate_key() {
+    function generate_key($id) {
         $randomv = rand().rand().rand();
-        $new_key = hash('sha256', $randomv);
+
+        $new_key = $id.hash('sha256', $randomv);
         $new_key .= microtime(true);
         return $new_key;
 }
 
-//генерирует количество ключей, равное переменной $number, на выходе получаем все ключи в массиве 
-    function createkeys($number) {
+//генерирует количество ключей, равное переменной $number, на выходе получаем все ключи в массиве
+    function createkeys($number,$id) {
         $i = 0;
         $array='';
          while ($i<$number) {
-             $key=generate_key();
+             $key=generate_key($id);
              addkey(get_db_connect(),$key);
              $array  .= "$key<br>\n";
              $i++;
