b93e5e6f1dbdb167371085adfde867f53a53dd56
ptroyanov <paul.troyanov@gmail.com>
2015-06-18 17:10:31 +0300

    ;-(

--- cancelRequest.php
+++ cancelRequest.php
@@ -1,24 +1,8 @@
 <?php
+    include('pdo.php');
 
-    function sendRequest ($key_info ,$info, $address, $secret_key = null){
-        $url = $address;
-        $fields = array(
-            $key_info => $info,
-            'secret_key' => $secret_key
-        );
-
-        $fields_str = '';
-        foreach($fields as $key=>$value) { $fields_str .= $key.'='.$value.'&'; }
-        $fields_str = trim($fields_str, '&');
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_str);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        return $response;
-    }
+    var_dump($_POST);
+    exit();
 
     $keys = array();
     foreach($_POST as $key => $value) {
