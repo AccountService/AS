de7284e5c84945ab1005350a35ba245db084c8be
ptroyanov <paul.troyanov@gmail.com>
2015-06-24 18:41:09 +0300

    modified adapter class

--- AdapterClass.php
+++ AdapterClass.php
@@ -1,4 +1,33 @@
 <?php
+
+    abstract Class dbConnector
+    {
+
+        public function queryExecute($db,$request, $arrayOfParams = null){
+
+            $query = $db->prepare("$request");
+            if($arrayOfParams !=null){
+
+                foreach ($arrayOfParams as $key => $value){
+
+                    $query->bindValue(":$key",$value, PDO::PARAM_STR);
+
+                }
+            }
+
+            $query->execute();
+            return $query;
+        }
+
+        public function fetcher($query) {
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+                $answer[] = $row['gen_key'];
+            }
+
+            return $answer;
+        }
+
+    }
     class Adapter {
         private $db;
 
