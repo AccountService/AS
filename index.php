570e2c65557f67e1f0eb5f5ca9905627f70b2dcd
ptroyanov <paul.troyanov@gmail.com>
2015-06-25 19:16:03 +0300

    it is all now in OOP style!

--- index.php
+++ index.php
@@ -1,192 +1,209 @@
 <!DOCTYPE html>
 <html >
-  <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" charset="utf-8" />
-      <link rel="stylesheet" href="css/style.css">
-     <?php include('pdo.php');
-      $jsondata = file_get_contents("products.json");
-      $json = json_decode($jsondata, true);
-      session_start();
-     if(isset($_GET['exit'])) {
-         session_destroy();
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css">
+
+    <?php
+    include_once('AdapterClass.php');
+
+    $DB = new db();
+
+    $jsondata = file_get_contents("products.json");
+    $json = json_decode($jsondata, true);
+
+    session_start();
+
+    if(isset($_GET['exit'])) {
+        session_destroy();
 
         echo '<script>
-                document.location.href = "index.php";
-             </script>';
-     }
+        document.location.href = "index.php";
+        </script>';
+    }
     ?>
 
+</head>
 
-  </head>
+<body>
 
-  <body>
-   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-   <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 
     <section class="wrapper" style="float:left;">
 
-  <section class="material-design-hamburger">
-    <button class="material-design-hamburger__icon">
-      <span class="material-design-hamburger__layer"></span>
-    </button>
-  </section>
+        <section class="material-design-hamburger">
+            <button class="material-design-hamburger__icon">
+            <span class="material-design-hamburger__layer"></span>
+            </button>
+        </section>
 
-  <section class="menu menu--off">
+        <section class="menu menu--off">
+            <?php
+            if(isset($_SESSION['name'])) { ?>
+                <div><a href="index.php">Products</a></div>
+                <div><a href="index.php?keys">My keys</a></div>
+                <div><a href="index.php?exit">Exit</a></div>
 
-      <?php if(isset($_SESSION['name'])) { ?>
-          <div><a href="index.php">Products</a></div>
-          <div><a href="index.php?keys">My keys</a></div>
-      <div><a href="index.php?exit">Exit</a></div>
+            <?php
+            } else { ?>
+                <div><a href="index.php">Products</a></div>
+                <div><a href="signup.php">Registration</a></div>
+            <?php
+            } ?>
+        </section>
 
-      <?php } else { ?>
-      <div><a href="index.php">Products</a></div>
-      <div><a href="signup.php">Registration</a></div>
-      <?php } ?>
-  </section>
+    </section>
 
-</section>
-<div style="float:left;">
+    <div class="container-form">
 
-</div>
- <div class="container-form">
+    <?php
+    if(isset($_SESSION['name'])) {
+        $name = $_SESSION['name'];
+        echo "<h2>Hello, $name</h2>";
+    } else {?>
+        <form method="POST">
 
-  <?php if(isset($_SESSION['name'])) {
-      $name = $_SESSION['name'];
-      echo "<h2>Hello, $name</h2>";
-  } else {?>
+            <div class="group left-move">
+                <input type="text" name="login" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>e-mail:</label>
+            </div>
 
+            <div class="group left-move">
+                <input type="password" name="pass" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>password:</label>
+            </div>
 
-      <form method="POST">
+            <div class="left-move button-move1">
+                <button class="btn waves-effect waves-light floating" type="submit" name="action">LOG IN</button>
+            </div>
 
-          <div class="group left-move">
-              <input type="text" name="login" required>
-              <span class="highlight"></span>
-              <span class="bar"></span>
-              <label>e-mail:</label>
-          </div>
+            <br><br>
 
-          <div class="group left-move">
-              <input type="password" name="pass" required>
-              <span class="highlight"></span>
-              <span class="bar"></span>
-              <label>password:</label>
-          </div>
-          <div class="left-move button-move1">
-              <button class="btn waves-effect waves-light floating" type="submit" name="action">LOG IN</button>
-          </div>
-          <br><br>
+            <a style="margin-left:30px;" href="signup.php">sign-up</a>
 
-          <a style="margin-left:30px;" href="signup.php">sign-up</a>
+        </form>
+    <?php
+    }?>
 
+    </div>
 
-      </form>
- <?php }?>
+    <br>
 
+    <div class="container-main">
+    <?php
+    if(isset($_POST['login']) && isset($_POST['pass'])) {
+        if($DB->isAuthorize($_POST['login'], $_POST['pass'])) {
 
-</div>
-<br>
-	 <div class="container-main">
-         <?php
-         if(isset($_POST['login']) && isset($_POST['pass'])) {
-             if(isAuthorize(get_db_connect(),$_POST['login'], $_POST['pass'])) {
+            $_SESSION['logged']=true;
+            $_SESSION['id'] = $DB->getUserId($_POST['login']);
+            $_SESSION['name'] = $DB->getUserName($_SESSION['id']);
+            echo '<script>
+            document.location.href = "index.php";
+            </script>';
 
-                 $_SESSION['logged']=true;
-                 $_SESSION['id'] = getUserId(get_db_connect(), $_POST['login']);
-                 $_SESSION['name'] = getUserName(get_db_connect(), $_SESSION['id']);
-                 echo '<script>
-                document.location.href = "index.php";
-             </script>';
-             } else {echo "<center><h3>Wrong email or password!</h3><br><br></center>";}
+        } else {
+            echo "<center><h3>Wrong email or password!</h3><br><br></center>";
+        }
 
-         }
-         ?>
-		<center>
-
-
-            <?php if(isset($_GET['keys'])) {
-                echo ' <form action="" method="post">
+    }
+    ?>
+    <center>
+    <?php
+    if(isset($_GET['keys'])) {
+        echo '
+            <form action="" method="post">
 
                 Validate your key:
                 <input type="text" name="key" placeholder="license key">
 
-                <div class="center-button">  <button class="btn waves-effect waves-light floating" type="submit" name="key_subm">Validate</button></div>
-
+                <div class="center-button">
+                    <button class="btn waves-effect waves-light floating" type="submit" name="key_subm">Validate</button>
+                </div>
 
             </form>';
-                echo '<form action="cancelRequest.php" method="post">';
-                $keys = getAllBuyedKeys($_SESSION['id'], get_db_connect());
+
+        echo '
+            <form action="cancelRequest.php" method="post">';
+                $keys = $DB->getAllBuyedKeys($_SESSION['id']);
+
                 foreach ($keys as $key => $value) :
-                 echo  "<h5>".$key.":"."</h5>" ;
-                echo "<ul>";
-                foreach($value as $key) {
-                    $id = getIDbyKey(get_db_connect(), $key);
-                    echo "<li>ID: $id | Key: $key<input type='checkbox' name=\"$key\" value=\"$id\" class='checkbox'></li>";
-                }
-                echo "</ul>";
-                echo "<br>";
+                    echo  "<h5>".$key.":"."</h5>" ;
+                    echo "<ul>";
+
+                    foreach($value as $key) {
+                        $id = $DB->getIDbyKey($key);
+                        echo "<li>ID: $id | Key: $key<input type='checkbox' name=\"$key\" value=\"$id\" class='checkbox'></li>";
+                    }
+
+                    echo "</ul>";
+                    echo "<br>";
+
                 endforeach;
+
                 echo "<input type='text' name='Amount' value='100' style='width: 30px; display:inline-block'>
-                <span style='display:inline-block'>%</span><div class='center-button'>
-                <button class='btn waves-effect waves-light floating' type='submit'>Cancel request</button></div>";
-                echo "</form>"?>
+                      <span style='display:inline-block'>%</span><div class='center-button'>
+                      <button class='btn waves-effect waves-light floating' type='submit'>Cancel request</button></div>";
+        echo "</form>"?>
 
-            <div>
-
-    <?php
-        if(isset($_POST['key'])) {
-            //var_dump($_POST);
-            //var_dump($_SESSION['id']);
-            keyValidation($_POST['key']);
-
-
-            connectKeyToUser($_POST['key'], $_SESSION['id']);
-            header('Location: index.php?keys');
-        }
-    ?>
-
-     </div>
-
-          <?php  } else {
-
-            $i = 0;
-            foreach ($json['products'] as $key => $value) : ?>
-                <form action="http://10.55.33.35/product.php" method="get" class="form-selector">
-                    <input type="hidden" name="id" value="<?=$value['id']?>">
-                    <div class="dialog" >
-
-                        <div class="content">
-                            <div class="title"><?=$value['name']?></div><br>
-                            <div><?=$value['description']?></div>
-                            Count:
-                        </div>
-                            <input type="text" style="width:100px; margin-top:-70px; text-align:center" value="1" name="count">
-                        <button class="btn waves-effect waves-light floating" type="submit">$<?=(float)$value['price']?>
-
-                        </button>
-
-                        <button class="btn waves-effect waves-light" type="submit">Buy
-
-                        </button>
-
-
-                    </div>
-                </form>
+        <div>
 
             <?php
-                $i++;
-                if ($i%2==0) {echo '<br><br><br>';}
+            if(isset($_POST['key'])) {
+                $DB->keyValidation($_POST['key']);
+                $DB->connectKeyToUser($_POST['key'], $_SESSION['id']);
+                header('Location: index.php?keys');
+            }
+            ?>
 
-            endforeach; }?>
-        </center>
+        </div>
 
-     </div>
+    <?php
+    } else {
 
-        <script src="js/index.js"></script>
+        $i = 0;
+        foreach ($json['products'] as $key => $value) : ?>
+            <form action="http://10.55.33.35/product.php" method="get" class="form-selector">
+                <input type="hidden" name="id" value="<?=$value['id']?>">
+                <div class="dialog" >
+
+                    <div class="content">
+                    <div class="title"><?=$value['name']?></div><br>
+                    <div><?=$value['description']?></div>
+                    Count:
+                    </div>
+                    <input type="text" style="width:100px; margin-top:-70px; text-align:center" value="1" name="count">
+                    <button class="btn waves-effect waves-light floating" type="submit">$<?=(float)$value['price']?>
+                    </button>
+
+                    <button class="btn waves-effect waves-light" type="submit">Buy
+                    </button>
+
+                </div>
+            </form>
+
+            <?php
+            $i++;
+            if ($i%2==0) {echo '<br><br><br>';}
+
+        endforeach;
+    }?>
+
+    </center>
+
+    </div>
+
+    <script src="js/index.js"></script>
 
     <link rel="import" href="https://www.polymer-project.org/0.5/components/paper-ripple/paper-ripple.html">
-  <link rel="import" href="http://www.polymer-project.org/components/core-icons/core-icons.html">
-  <link rel="import" href="http://www.polymer-project.org/components/font-roboto/roboto.html">
+    <link rel="import" href="http://www.polymer-project.org/components/core-icons/core-icons.html">
+    <link rel="import" href="http://www.polymer-project.org/components/font-roboto/roboto.html">
 
-  </body>
+</body>
 </html>
 
