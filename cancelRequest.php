fa6c256fcf779b716c5c684f13af79b7a0792672
ptroyanov <paul.troyanov@gmail.com>
2015-06-26 18:11:36 +0300

    cancel request mod

--- cancelRequest.php
+++ cancelRequest.php
@@ -2,27 +2,8 @@
 
 session_start();
 
-include('pdo.php');
-
-function sendKeyId ($key_info, $info, $address, $secret_key = null){
-    $url = $address;
-    $fields = array(
-        $key_info => $info,
-        'secret_key' => $secret_key
-    );
-
-    $fields_str = '';
-    foreach($fields as $key=>$value) { $fields_str .= $key.'='.$value.'&'; }
-    $fields_str = trim($fields_str, '&');
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_str);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    return $response;
-}
+include('AdapterClass.php');
+$DB = new db();
 
 $returnedInfo = array();
 
@@ -33,7 +14,7 @@ foreach($_POST as $key => $value){
 $amount = array_pop($returnedInfo);
 
 $cancel_info = array (
-    'email' => getUserEmail(get_db_connect(), $_SESSION['id']),
+    'email' => $DB->getUserEmail($_SESSION['id']),
     'key_id' => $returnedInfo,
     'amount' => $amount
 );
@@ -46,7 +27,6 @@ $info = json_encode($cancel_info);
 echo $info;
 
 
-$answer = sendKeyId('cancel_info', $info, '10.55.33.27/dev/addRefund.php');
-
+$answer = $DB->sendData('cancel_info', $info, '10.55.33.27/dev/addRefund.php');
 
 echo $answer;
