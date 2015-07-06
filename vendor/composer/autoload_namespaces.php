0b339d21d6cb50ae015bbf6d6363b59a682baab9
Vlad Leikih <leikih@gmail.com>
2015-07-06 19:49:33 +0300

    installed PHPixie

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PHPixie' => array($vendorDir . '/phpixie/core/classes', $vendorDir . '/phpixie/db/classes', $vendorDir . '/phpixie/orm/classes'),
     '' => array($baseDir . '/src'),
 );
